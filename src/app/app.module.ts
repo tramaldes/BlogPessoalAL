c629054f841753569b41f8a01df23c3e7dbd6bfd
Thiago Ramaldes Santos <tramaldes.s@gmail.com>
2021-07-02 21:43:31 -0300

    Atividade Blog_pessoal Atualizado até v.6 CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,8 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +19,20 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    InicioComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://localhost:8080//temas', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://localhost:8080//temas', tema, this.token)
+  }
+}
